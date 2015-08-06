a92bdfbf7221024f4b768b544cb180a5580cc195
yodincova <odincova1024@gmail.com>
2015-08-06 11:57:03 +0300

    Initial Commit

---
+++ DinamicTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DinamicTable
+//
+//  Created by Juliya on 06.08.15.
+//  Copyright (c) 2015 Juliya Odincova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DinamicTable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DinamicTable
+//
+//  Created by Juliya on 06.08.15.
+//  Copyright (c) 2015 Juliya Odincova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
